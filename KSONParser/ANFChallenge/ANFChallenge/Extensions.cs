11319c8a593ed7e7eda318cb47f5af9b0119ed97
kunrex <74914319+kunrex@users.noreply.github.com>
2021-05-04 17:16:29 +0530

    Add files via upload

---
+++ KSONParser/ANFChallenge/ANFChallenge/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ANFChallenge
+{
+    public static class Extensions
+    {
+        public static bool IsViableToBeParsed(this char _char)
+        {
+            return char.IsLetter(_char) || char.IsNumber(_char) || _char == '"' || _char == '.' || _char == ',' || _char == '[' || _char == ']';
+        }
+    }
+}
