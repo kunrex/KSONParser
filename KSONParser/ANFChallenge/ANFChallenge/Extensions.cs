2e9c6aed2107aeea64dfc30ac27e8991b460c7aa
kunrex <74914319+kunrex@users.noreply.github.com>
2021-05-14 14:15:56 +0530

    Update Extensions.cs

--- KSONParser/ANFChallenge/ANFChallenge/Extensions.cs
+++ KSONParser/ANFChallenge/ANFChallenge/Extensions.cs
@@ -1,11 +1,21 @@
-﻿using System;
-namespace ANFChallenge
+using System;
+namespace HelpfulExtensions
 {
     public static class Extensions
     {
         public static bool IsViableToBeParsed(this char _char)
         {
             return char.IsLetter(_char) || char.IsNumber(_char) || _char == '"' || _char == '.' || _char == ',' || _char == '[' || _char == ']';
+        }
+
+        public static char ToChar(this string _string)
+        {
+            if (_string.Length < 0)
+                throw new ArgumentException("String has a length of more than 0");
+            else if(_string == "" || _string == string.Empty)
+                throw new ArgumentException("String is empty");
+            else
+                return _string[0];
         }
     }
 }
