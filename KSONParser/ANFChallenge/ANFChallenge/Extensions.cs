9fe0ef03c6e3959d7ff7d6be4188dcb3301fa0fb
kunrex <74914319+kunrex@users.noreply.github.com>
2021-05-14 14:49:26 +0530

    Update Extensions.cs

--- KSONParser/ANFChallenge/ANFChallenge/Extensions.cs
+++ KSONParser/ANFChallenge/ANFChallenge/Extensions.cs
@@ -3,12 +3,21 @@ namespace HelpfulExtensions
 {
     public static class Extensions
     {
-        public static bool IsViableToBeParsed(this char _char)
+        public static bool IsViableToBeSerialised(this char _char) => char.IsLetter(_char) || char.IsNumber(_char) || _char == '"' || _char == '.' || _char == ',' || _char == '[' || _char == ']';
+
+        public static bool IsValidToBeDeserialised_Array(this char _char) => char.IsLetter(_char) || char.IsNumber(_char) || _char == '.';
+
+        public static bool DeterminesEndOfSerialisedArray(this char _char) => _char == ',' || _char == ']';
+
+        public static bool IsValidType(this Type type, bool considerBool = false)
         {
-            return char.IsLetter(_char) || char.IsNumber(_char) || _char == '"' || _char == '.' || _char == ',' || _char == '[' || _char == ']';
+            if (type.IsArray)
+                return type.GetElementType() == typeof(int) || type.GetElementType() == typeof(float) || type.GetElementType() == typeof(string) || type.GetElementType() == typeof(char) || (type.GetElementType() == typeof(bool) && considerBool);
+            else
+                return type == typeof(int) || type == typeof(float) || type == typeof(string) || type == typeof(char) || (type == typeof(bool) && considerBool);
         }
 
-        public static char ToChar(this string _string)
+        public static char ToChar(this string _string) 
         {
             if (_string.Length < 0)
                 throw new ArgumentException("String has a length of more than 0");
