fabfbb83a4a8815f1365653aa63ef45ee3664ec2
Kunal <74914319+kunrex@users.noreply.github.com>
2021-05-19 13:16:18 +0530

    Create Serialisable.cs

---
+++ KSONParser/ANFChallenge/ANFChallenge/Serialisable.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace HelpfulExtensions
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class Serialisable : Attribute
+    {
+        public string Serialise(int value) => value.ToString();
+
+        public string Serialise(float value) => value.ToString();
+
+        public string Serialise(string value) => value;
+
+        public string Serialise(char value) => value.ToString();
+
+        public string Serialise(bool value) => value.ToString().ToUpper();
+
+        public dynamic Deserialise(string value, Type type)
+        {
+            if(!type.IsValidType(true))
+                throw new InvalidTypeException(false);
+
+            switch(type)
+            {
+                case Type t when t == typeof(int):
+                    return int.Parse(value);
+                case Type t when t == typeof(float):
+                    return float.Parse(value);
+                case Type t when t == typeof(bool):
+                    return value == "TRUE";
+                case Type t when t == typeof(string):
+                    return value;
+                case Type t when t == typeof(char):
+                    return value.ToChar();
+            }
+
+            return null;
+        }
+    }
+}
